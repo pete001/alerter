a8281af6efdb5614219c65cf7c2880d31bb66ef3
Pete Cheyne <pete.cheyne@performancehorizon.com>
2016-01-08 16:36:54 +0000

    Adds setter.

--- src/Persistence/Laravel/Repository/AlertRepository.php
+++ src/Persistence/Laravel/Repository/AlertRepository.php
@@ -1,11 +1,18 @@
 <?php namespace Pete001\Alerter\Persistence\Laravel\Repository;
 
 use Pete001\Alerter\Domain\Repository\AlertRepositoryInterface;
+use \Mother\Core\Domain\Entity\AlertGroup;
 
 class AlertRepository extends AbstractRepository implements AlertRepositoryInterface
 {
 	public function getAlertGroup()
 	{
 		return $this->model->alertGroup();
+	}
+
+	public function setAlertGroup(AlertGroup $alertGroup)
+	{
+		$this->model->alert_group_id = $alertGroup->id;
+		return $this;
 	}
 }
