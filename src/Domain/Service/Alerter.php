94d143eafc59feda0687a3763801147eaf9c19c5
Pete Cheyne <pete.cheyne@performancehorizon.com>
2016-01-12 20:50:21 +0000

    Each function fetches an individual object.

--- src/Domain/Service/Alerter.php
+++ src/Domain/Service/Alerter.php
@@ -25,6 +25,8 @@ class Alerter implements AlerterInterface
     private $alertDetailRepository;
     private $alertStrategy;
 
+    private $registry = [];
+
     public function __construct(
         AlertRepositoryInterface $alertRepository,
         AlertRequirementRepositoryInterface $alertRequirementRepository,
@@ -55,21 +57,99 @@ class Alerter implements AlerterInterface
      */
     public function fire($userId, $hook, $message)
     {
-        $hook = $this->alertHookRepository->where('title', '=', $hook)->firstOrFail();
+        $result = [];
+        $this->initRegistry($userId, $hook);
 
-        $registryAlerts = $this->alertRegistryRepository
+        foreach ($this->registry as $registry) {
+            $alert = $this->getAlert($registry);
+            $group = $this->getGroup($alert);
+            $details = $this->getDetails($registry);
+            $service = $group->create($alert, ...$details);
+            $result[] = $service->send($message);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Initialise the registry which returns the alerts
+     *
+     * @param String $userId  The generic user id who is triggering the alert(s)
+     * @param String $hook    The name of the hook
+     *
+     * @return Array
+     */
+    private function initRegistry($userId, $hook)
+    {
+        return $this->registry = $this->getRegistry(
+            $userId,
+            $this->getHook($hook)
+        );
+    }
+
+    /**
+     * Fetch the individual alert
+     *
+     * @param String $userId  The generic user id who is triggering the alert(s)
+     * @param String $hook    The name of the hook
+     *
+     * @return Object
+     */
+    private function getRegistry($userId, $hook)
+    {
+        return $this->alertRegistryRepository
             ->where('users_id', '=', $userId)
             ->where('alert_hook_id', '=', $hook->id)
             ->getAll();
+    }
 
-        foreach ($registryAlerts as $registryAlert) {
-            $alert = $this->alertRepository->begin();
-            $alert = $this->alertRepository->with('alertGroup')->getById($registryAlert->alert_id);
-            $detail = $this->alertDetailRepository->begin();
-            $detail = $this->alertDetailRepository->with('alertRequirement')->where('alert_registry_id', '=', $registryAlert->id)->getAll();
-            $group = $this->alertStrategy->initialise($alert->getAlertGroup());
-            $service = $group->create($alert, ...$detail);
-            $result = $service->send($message);
-        }
+    /**
+     * Fetch the hook object
+     *
+     * @param String $hook The name of the hook
+     *
+     * @return Object
+     */
+    private function getHook($hook)
+    {
+        return $this->alertHookRepository->where('title', '=', $hook)->firstOrFail();
+    }
+
+    /**
+     * Fetch the alert object
+     *
+     * @param Object $registryAlert The registry object
+     *
+     * @return Object
+     */
+    private function getAlert($registryAlert)
+    {
+        $alert = $this->alertRepository->begin();
+        return $this->alertRepository->with('alertGroup')->getById($registryAlert->alert_id);
+    }
+
+    /**
+     * Fetch the alert details
+     *
+     * @param Object $registryAlert The registry object
+     *
+     * @return Array
+     */
+    private function getDetails($registryAlert)
+    {
+        $detail = $this->alertDetailRepository->begin();
+        return $this->alertDetailRepository->with('alertRequirement')->where('alert_registry_id', '=', $registryAlert->id)->getAll();
+    }
+
+    /**
+     * Fetch the alert group
+     *
+     * @param Object $alert The alert object
+     *
+     * @return Object
+     */
+    private function getGroup($alert)
+    {
+        return $this->alertStrategy->initialise($alert->getAlertGroup());
     }
 }
